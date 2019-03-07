790f44cf216e5837a75bc5d474ff9c9babc294d5
Qizi <32046016+531387937@users.noreply.github.com>
2019-03-07 19:07:06 +0800

    别踩白块！

---
+++ Chicken_Attack/Assets/Scripts/Black_White.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Black_White : MonoBehaviour
+{
+    public GameObject White_Block;
+    public GameObject Black_Block;
+    private ArrayList blocks;
+    public GameObject BlockArea;
+    private float timer;
+    private bool GameStart=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameStart)
+        {
+            timer += Time.deltaTime;
+        }
+    }
+    public void StartGame()
+    {
+        blocks = new ArrayList();
+        for(int i = 0;i<=3;++i)
+        {
+            AddBlock(i);
+        }
+    }
+    void AddBlock(int rowIndex)
+    {
+        int WhiteBlockNum = Random.Range(0, 2);
+        for(int i = 0;i<2;i++)
+        {
+            GameObject o;
+            if(i==WhiteBlockNum)
+            {
+                o = Instantiate(White_Block) as GameObject;
+            }
+            else
+            {
+                o = Instantiate(Black_Block) as GameObject;
+            }
+            o.transform.parent = BlockArea.transform;
+            Block b = o.GetComponent<Block>();
+            b.SetPosition(i, rowIndex);
+            blocks.Add(b);
+        }
+    }
+    public void SelectBlock(Block block)
+    {
+        if(block.num==0)
+        {
+            if(block.CanTouch)
+            {
+                for(int i = 0;i<blocks.Count;i++)
+                {
+                    Block b = (Block)blocks[i];
+                    b.MoveDown();
+
+                    if (b.num == -1)
+                    {
+                        blocks.RemoveAt(i);
+                        Destroy(b.gameObject);
+                        i--;
+                    }
+                }
+                AddBlock(3);
+            }
+        }
+    }
+}
---
+++ Chicken_Attack/Assets/Scripts/Block.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public bool CanTouch;
+    public Black_White BW;
+    public int num;
+    private int xx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BW = GameObject.FindObjectOfType<Black_White>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        BW.SelectBlock(this);
+    }
+    public void SetPosition(int x,int y)
+    {
+        xx = x;
+        num = y;
+        switch(x)
+        {
+            case 0:
+                transform.position = new Vector3(-7, transform.position.y, 0);
+                break;
+            case 1:
+                transform.position = new Vector3(7, transform.position.y, 0);
+                break;
+        }
+        switch(y)
+        {
+            case 0:
+                transform.position = new Vector3(transform.position.x, -3, 0);
+                break;
+            case 1:
+                transform.position = new Vector3(transform.position.x, -1, 0);
+                break;
+            case 2:
+                transform.position = new Vector3(transform.position.x, 1, 0);
+                break;
+            case 3:
+                transform.position = new Vector3(transform.position.x, 3, 0);
+                break;
+        }
+    }
+    public void MoveDown()
+    {
+        num--;
+        SetPosition(xx, num);
+        //transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(transform.position.x, transform.position.y - 2, 0), 20f*Time.deltaTime);
+        //--num;
+    }
+}
