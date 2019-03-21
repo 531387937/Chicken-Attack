f9dd584f91218e3eb6dda28da9140beda2ee1acc
Qizi <32046016+531387937@users.noreply.github.com>
2019-03-21 10:21:27 +0800

    我死了

--- Chicken_Attack/Assets/Scripts/UI/GridImage.cs
+++ Chicken_Attack/Assets/Scripts/UI/GridImage.cs
@@ -11,6 +11,8 @@ public class GridImage : MonoBehaviour
 
     public static Action OnExit;
     public static Action<Transform> OnDrag;
+    public int _time=0;
+   public float timer;
 
     //public void OnPointerEnter(PointerEventData eventData)
 
@@ -29,17 +31,20 @@ public class GridImage : MonoBehaviour
         if (OnEnter != null)
         {
             OnEnter(transform);
+            
         }
     }
     private void OnMouseDrag()
     {
-        if(OnDrag!=null)
+        
+        if (OnDrag!=null)
         {
             OnDrag(transform);
+           
         }
     }
     public void OnMouseExit()
-    {
+    { 
         if (OnExit != null)
 
         {
@@ -61,5 +66,6 @@ public class GridImage : MonoBehaviour
     //    }
 
     //}
+
 
 }
--- Chicken_Attack/Assets/Scripts/UI/UIManager.cs
+++ Chicken_Attack/Assets/Scripts/UI/UIManager.cs
@@ -7,11 +7,20 @@ public class UIManager : Singleton<UIManager>
     public ToolTip toolTip;
     private bool isShow = false;
     private bool isDrag = false;
+    [SerializeField]
+    private GridImage gridImage;
+    private bool Showing = false;
     private void OnEnable()
     {
         GridImage.OnEnter += GridImage_OnEnter;
         GridImage.OnExit += GridImage_OnExit;
         GridImage.OnDrag += GridImage_OnDrag;
+    }
+    private void OnDestroy()
+    {
+        GridImage.OnEnter -= GridImage_OnEnter;
+        GridImage.OnExit -= GridImage_OnExit;
+        GridImage.OnDrag -= GridImage_OnDrag;
     }
     // Start is called before the first frame update
     void Start()
@@ -22,6 +31,11 @@ public class UIManager : Singleton<UIManager>
     // Update is called once per frame
     void Update()
     {
+        if(Showing)
+        {
+            gridImage.timer += Time.deltaTime;
+            print("AAAAAAAAAAAA");
+        }
         if(isShow)
         {
             Vector2 position;
@@ -33,15 +47,27 @@ public class UIManager : Singleton<UIManager>
     }
     private void GridImage_OnEnter(Transform obj)
     {
-        Chicken c = obj.gameObject.GetComponent<ChiCken_State>().ThisChicken;
+        gridImage = obj.gameObject.GetComponent<GridImage>();
+        Showing = true;
+        gridImage._time++;
+        if (Showing && gridImage.timer <= 0.3f && gridImage._time == 2)
+        {
+            Showing = false;
+            isShow = true;
+            Chicken c = obj.gameObject.GetComponent<ChiCken_State>().ThisChicken;
         string text = GetTooltipText(c);
         toolTip.UpdateTolTip(text);
-        isShow = true;
+        }
     }
     private void GridImage_OnExit()
     {
+
+
+        Showing = false;
         isShow = false;
         toolTip.Hide();
+        gridImage._time = 0;
+           gridImage.timer = 0;
     }
     private string GetTooltipText(Chicken chicken)
     {
@@ -51,5 +77,8 @@ public class UIManager : Singleton<UIManager>
     private void GridImage_OnDrag(Transform obj)
     {
         isDrag = true;
+        Vector3 pos;
+        pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        obj.position = new Vector3(pos.x, pos.y, 0);
     }
 }
