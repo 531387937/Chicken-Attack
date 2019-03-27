858fb9237dfc4a4089f83a07557ccde48c496cca
xuyue1998 <32046003+xuyue1998@users.noreply.github.com>
2019-03-27 13:11:14 +0800

    陀螺仪控制左右移动场景（两模式可选，移动摄像机或者移动场景）

---
+++ Chicken_Attack/Assets/Scripts/Gyro.cs
@@ -0,0 +1,82 @@
+﻿
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+//陀螺仪控制图片移动
+public class Gyro : MonoBehaviour
+{
+    public Mode mode;
+    public float Speed = 2f;
+    public float limitPosX;
+    private float startPosX;
+    private Vector3 currentPos;
+
+    public enum Mode
+    {
+        CamMode,
+        PlaneMode
+    }
+
+    protected void Start()
+    {
+        startPosX = transform.position.x;
+        Input.gyro.enabled = true;
+        Input.gyro.updateInterval = 0.1f;
+    }
+
+    protected void Update()
+    {
+        switch (mode)
+        {
+           case Mode.CamMode:
+                if (Mathf.Abs(startPosX) + Mathf.Abs(currentPos.x) > limitPosX)
+                {
+                    if (Input.gyro.attitude.ToEuler().x < 0)
+                    {
+                        transform.position = currentPos + new Vector3(0.5f, 0, 0);
+                    }
+                    if (Input.gyro.attitude.ToEuler().x > 0)
+                    {
+                        transform.position = currentPos - new Vector3(0.5f, 0, 0);
+                    }
+                    currentPos = transform.position;
+                    return;
+                }
+                if (Mathf.Abs(Input.gyro.attitude.ToEuler().x) > 0.35)
+                {
+                    this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, this.transform.localPosition
+                        + new Vector3(Input.gyro.attitude.ToEuler().x, 0, 0), Speed * Time.deltaTime);
+                }
+               break;
+           case Mode.PlaneMode:
+                if (Mathf.Abs(startPosX) + Mathf.Abs(currentPos.x) > limitPosX)
+                {
+                    if (Input.gyro.attitude.ToEuler().x < 0)
+                    {
+                        transform.position = currentPos - new Vector3(0.5f, 0, 0);
+                    }
+                    if (Input.gyro.attitude.ToEuler().x > 0)
+                    {
+                        transform.position = currentPos + new Vector3(0.5f, 0, 0);
+                    }
+                    currentPos = transform.position;
+                    return;
+                }
+                if (Mathf.Abs(Input.gyro.attitude.ToEuler().x) > 0.35)
+                {
+                    this.transform.localPosition = Vector3.MoveTowards(this.transform.localPosition, this.transform.localPosition
+                        - new Vector3(Input.gyro.attitude.ToEuler().x, 0, 0), Speed * Time.deltaTime);
+                }
+               break;
+        }
+        currentPos = this.transform.position;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(50, 100, 500, 20), "Label : " + Input.gyro.attitude.ToEuler().x);
+    }
+
+
+}
+
