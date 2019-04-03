d473617eb44847bffd10bbe9a5eff16efddd22fc
Qizi <531387937@qq.com>
2019-04-04 02:50:36 +0800

    鸡动画需全部重做

---
+++ Chicken_Attack/Assets/Scripts/Chicken/ChickenAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAI : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+    }
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
