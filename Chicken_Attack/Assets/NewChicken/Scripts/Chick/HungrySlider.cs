bcdf2c1304ec4b60fee7644e726e854c857079b9
xuyue1998 <32046003+xuyue1998@users.noreply.github.com>
2019-05-19 15:59:50 +0800

    加入了饥饿值slider，和名字UI放在一起了，对训练的浮动就可以在训练端对参加训练的鸡的饥饿值进行判断。

---
+++ Chicken_Attack/Assets/NewChicken/Scripts/Chick/HungrySlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HungrySlider : MonoBehaviour
+{
+    public FightChicken thisChicken;
+    public Slider HungryStrip;   
+
+    void Start()
+    {
+        HungryStrip.maxValue = 100;
+        StartCoroutine(Hungry());
+    }
+
+    private void Update()
+    {
+        Debug.Log("thisChicken.Hungry" + thisChicken.Hungry);
+        HungryStrip.value = thisChicken.Hungry;
+    }
+
+    IEnumerator Hungry()
+    {
+        yield return new WaitForSeconds(60);
+        thisChicken.Hungry -= 2;
+        Debug.Log("饥饿值-2");
+    }
+}
--- Chicken_Attack/Assets/NewChicken/Scripts/FightChicken/MyFightChicken.cs
+++ Chicken_Attack/Assets/NewChicken/Scripts/FightChicken/MyFightChicken.cs
@@ -8,27 +8,45 @@ public class MyFightChicken : MonoBehaviour
     private GameObject GameUI;
     public GameObject Name_UI;
     private Vector3 Pos;
-    private GameObject a;
+    public GameObject Current_Name_UI;
     private void Start()
     {
-        Name_UI = Resources.Load("Name") as GameObject;
         GameUI = GameObject.Find("Canvas");
+        Name_UI = Resources.Load("Name") as GameObject;
         Pos = Camera.main.WorldToScreenPoint(this.transform.position);
-       a= Instantiate(Name_UI, Pos,Quaternion.identity,GameUI.transform);
+        Current_Name_UI = Instantiate(Name_UI, Pos,Quaternion.identity,GameUI.transform);
+        Current_Name_UI.GetComponentInChildren<TextMeshProUGUI>().text = self.Name;
+        Current_Name_UI.GetComponent<HungrySlider>().thisChicken = self;
         print(self.Name);
-        a.GetComponentInChildren<TextMeshProUGUI>().text = self.Name;
-        
+
         StartCoroutine(SavePos());
     }
+
     private void Update()
     {
-        Pos = new Vector3(Camera.main.WorldToScreenPoint(this.transform.position).x, Camera.main.WorldToScreenPoint(this.transform.position).y + 140f, 0);
-        a.transform.position = Pos;
+        Pos = new Vector3(Camera.main.WorldToScreenPoint(this.transform.position).x, Camera.main.WorldToScreenPoint(this.transform.position).y + 70f, 0);
+        Current_Name_UI.transform.position = Pos;
     }
+    
     IEnumerator SavePos()
     {
         self.Pos = this.transform.position;
         yield return new WaitForSeconds(5);
         StartCoroutine(SavePos());
+    }
+
+    private void OnDisable()
+    {
+        Current_Name_UI.SetActive(false);
+    }
+
+    private void OnEnable()
+    {
+        Current_Name_UI.SetActive(true);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(Current_Name_UI);
     }
 }
