b06426bf190adfa186c36549e6a934a92c6bcfe2
Qizi <531387937@qq.com>
2019-05-20 02:57:08 +0800

    更完善了繁殖界面的三个鸡蛋，差小鸡出现的表现形式

--- Chicken_Attack/Assets/NewChicken/Scripts/Chick/ChickenBreed.cs
+++ Chicken_Attack/Assets/NewChicken/Scripts/Chick/ChickenBreed.cs
@@ -5,14 +5,17 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 public class ChickenBreed : MonoBehaviour
 {
-    public RawImage ChooseChickenPic;
+    public GameObject[] Chicken;
     public RawImage BuyChickenPic;
     public Texture2D[] Pics;
     public GameObject NewChickUI;
-
+    public GameObject eggs;
+    public GameObject Pos;
+    private GameObject Breed_Chicken;
     void Start()
     {
-        ChooseChickenPic.texture = Pics[(int)GameSaveNew.Instance.playerChicken.Type];
+        Breed_Chicken = Instantiate(Chicken[(int)GameSaveNew.Instance.playerChicken.Type], Pos.transform.position, new Quaternion(0, 180, 0, 1));
+        Breed_Chicken.transform.localScale = new Vector3(1.3f, 1.3f, 1.3f);
         if (GameSaveNew.Instance.PD.ShopChicken != null)
         {
             BuyChickenPic.texture = Pics[(int)GameSaveNew.Instance.PD.ShopChicken.Type];
@@ -23,18 +26,28 @@ public class ChickenBreed : MonoBehaviour
         }
     }
 
+    public void Three_Eggs()
+    {
+        if (GameSaveNew.Instance.PD.ShopChicken != null)
+        {
+            eggs.SetActive(true);
+        }
+    }
+    public void Dis(GameObject otherEgg)
+    {
+        otherEgg.SetActive(false);
+    }
     //诞生新的鸡，以后加上命名功能
     public void Decided()
     {
-        if(GameSaveNew.Instance.playerChicken != null)
-        {
-            FightChicken NewChick = new FightChicken("Child", GameSaveNew.Instance.playerChicken, GameSaveNew.Instance.PD.ShopChicken);
+        //eggs.SetActive(false);
+            FightChicken NewChick = new FightChicken("小鸡", GameSaveNew.Instance.playerChicken, GameSaveNew.Instance.PD.ShopChicken);
+        GameSaveNew.Instance.PD.Chick = new List<FightChicken>();
             GameSaveNew.Instance.PD.Chick.Add(NewChick);
             //鸡诞生动画
-            NewChickUI.GetComponent<ShopChickenUI>().SetShopChickenUi(NewChick);
-            NewChickUI.SetActive(true);
+            //NewChickUI.GetComponent<ShopChickenUI>().SetShopChickenUi(NewChick);
+            //NewChickUI.SetActive(true);
             Debug.Log("生小鸡！！！！");
-        }
         //清空商店买的鸡
         GameSaveNew.Instance.PD.ShopChicken = null;
     }
