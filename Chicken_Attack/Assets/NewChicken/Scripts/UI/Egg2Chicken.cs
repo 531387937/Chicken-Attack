f15fb52965bcdaaec057bb357361df2ee3c8ada9
Qizi <531387937@qq.com>
2019-05-21 14:49:30 +0800

    修复，我去找音效了

---
+++ Chicken_Attack/Assets/NewChicken/Scripts/UI/Egg2Chicken.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg2Chicken : MonoBehaviour
+{
+    public GameObject a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ToChi()
+    {
+        a.SetActive(true);
+        this.gameObject.SetActive(false);
+    }
+}
