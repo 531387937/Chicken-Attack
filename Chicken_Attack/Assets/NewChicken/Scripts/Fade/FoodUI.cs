0e03bd75e75547e78f63b03d43a9013d77721940
xuyue1998 <32046003+xuyue1998@users.noreply.github.com>
2019-05-25 01:38:34 +0800

    主场景UI适配全分辨率，小鸡动画加入。

---
+++ Chicken_Attack/Assets/NewChicken/Scripts/Fade/FoodUI.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+namespace Script
+{
+    class FoodUI : MonoBehaviour
+    {
+        public Image Body;//ui元素的本体, panel等
+        public RectTransform CanvasRectangle; //image所在的画布
+        private bool dragging;
+        private Vector2 targetPosition;//移动时目标位置
+        private Vector2 offset;//开始移动前记录鼠标与body之间的偏移距离
+        public void Update()
+        {
+            if (dragging)
+            {
+                //以0.5倍的单位将ui向目标位置移动
+                Body.rectTransform.anchoredPosition += (targetPosition - Body.rectTransform.anchoredPosition) * 0.5f;
+            }
+        }
+        public void OnBeginDrag(BaseEventData baseEventData)
+        {
+            PointerEventData pointerEventData = baseEventData as PointerEventData;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                CanvasRectangle, pointerEventData.position, pointerEventData.pressEventCamera, out offset);
+            //计算偏移量
+            offset = Body.rectTransform.anchoredPosition - offset;
+            dragging = true;
+        }
+        public void OnDrag(BaseEventData baseEventData)
+        {
+            PointerEventData pointerEventData = baseEventData as PointerEventData;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                CanvasRectangle, pointerEventData.position, pointerEventData.pressEventCamera, out targetPosition);
+            //更新目标位置
+            targetPosition = targetPosition + offset;
+        }
+        public void OnEndDrag(BaseEventData baseEventData)
+        {
+            dragging = false;
+        }
+    }
+}
