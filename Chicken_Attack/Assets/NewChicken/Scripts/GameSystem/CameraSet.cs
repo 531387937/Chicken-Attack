7f8cc0879474d82220aa9a36226fc7627088cc46
xuyue1998 <32046003+xuyue1998@users.noreply.github.com>
2019-05-28 05:37:46 +0800

    主场景，商店，繁殖场景均添加多分辨率支持（按长适配）

---
+++ Chicken_Attack/Assets/NewChicken/Scripts/GameSystem/CameraSet.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSet : MonoBehaviour
+{
+
+    float devHeight = 6.4f;
+    float devWidth = 9.6f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        float screenHeight = Screen.height;
+
+        Debug.Log("screenHeight = " + screenHeight);
+
+        this.GetComponent<Camera>().orthographicSize = screenHeight / 100.0f;
+
+        float orthographicSize = this.GetComponent<Camera>().orthographicSize;
+
+        float aspectRatio = Screen.width * 1.0f / Screen.height;
+
+        float cameraWidth = orthographicSize * 2 * aspectRatio;
+
+        Debug.Log("cameraWidth = " + cameraWidth);
+
+        if (cameraWidth < devWidth)
+        {
+            orthographicSize = devWidth / (2 * aspectRatio);
+            Debug.Log("new orthographicSize = " + orthographicSize);
+            this.GetComponent<Camera>().orthographicSize = orthographicSize;
+        }
+
+    }
+}
