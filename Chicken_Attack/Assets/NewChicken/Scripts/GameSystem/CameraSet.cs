9eb1b9b336c5dd3ebc443e39821c8b780e99a49d
xuyue1998 <32046003+xuyue1998@users.noreply.github.com>
2019-05-28 06:03:49 +0800

    为Loading新增适配，适配策略优化，

--- Chicken_Attack/Assets/NewChicken/Scripts/GameSystem/CameraSet.cs
+++ Chicken_Attack/Assets/NewChicken/Scripts/GameSystem/CameraSet.cs
@@ -4,33 +4,22 @@ using System.Collections;
 public class CameraSet : MonoBehaviour
 {
 
-    float devHeight = 6.4f;
-    float devWidth = 9.6f;
+    float devWidth = 17.78f;
 
     // Use this for initialization
     void Start()
     {
-
-        float screenHeight = Screen.height;
-
-        Debug.Log("screenHeight = " + screenHeight);
-
-        this.GetComponent<Camera>().orthographicSize = screenHeight / 100.0f;
+        //this.GetComponent<Camera>().orthographicSize = screenHeight / 97.0f;
 
         float orthographicSize = this.GetComponent<Camera>().orthographicSize;
-
         float aspectRatio = Screen.width * 1.0f / Screen.height;
-
         float cameraWidth = orthographicSize * 2 * aspectRatio;
-
         Debug.Log("cameraWidth = " + cameraWidth);
-
-        if (cameraWidth < devWidth)
+        if (cameraWidth > devWidth)
         {
             orthographicSize = devWidth / (2 * aspectRatio);
             Debug.Log("new orthographicSize = " + orthographicSize);
             this.GetComponent<Camera>().orthographicSize = orthographicSize;
         }
-
     }
 }
