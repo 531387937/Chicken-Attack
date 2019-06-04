29025f74f3019e6721fb0f111c087a7592d62bde
Qizi <531387937@qq.com>
2019-06-04 18:36:51 +0800

    锁关方法加入

---
+++ Chicken_Attack/Assets/NewChicken/Scripts/GameSystem/LevelCtr.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelCtr : MonoBehaviour
+{
+    private Button[] levels;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levels = GetComponentsInChildren<Button>();
+        for(int i=0;i<levels.Length;i++)
+        {
+            if(i<=GameSaveNew.Instance.PD.NowLevel-1)
+            {
+                levels[i].interactable = true;
+            }
+            else
+            {
+                levels[i].interactable = false;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
