c6bb315eba54fa6715bc6b18c27ae6204523b4b1
Qizi <531387937@qq.com>
2019-06-15 00:44:02 +0800

    SR鸡喂食

--- Chicken_Attack/Assets/NewChicken/Scripts/GameSystem/HomeCtr.cs
+++ Chicken_Attack/Assets/NewChicken/Scripts/GameSystem/HomeCtr.cs
@@ -6,10 +6,14 @@ public class HomeCtr : MonoBehaviour
 {
     public Sprite[] Sp;
     public Vector3[] HomeScale;
+
+    public GameObject child;
+    public Sprite[] ChildSp;
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<SpriteRenderer>().sprite = Sp[GameSaveNew.Instance.PD.HomeLevel];
         gameObject.transform.localScale = HomeScale[GameSaveNew.Instance.PD.HomeLevel];
+        child.GetComponent<SpriteRenderer>().sprite = ChildSp[GameSaveNew.Instance.PD.HomeLevel];
     }
 }
