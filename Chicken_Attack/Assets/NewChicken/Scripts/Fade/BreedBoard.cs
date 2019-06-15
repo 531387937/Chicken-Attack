8485482775b4983c7caaa748431642c97c5f8271
xuyue1998 <32046003+xuyue1998@users.noreply.github.com>
2019-06-16 01:22:30 +0800

    2.4修复

---
+++ Chicken_Attack/Assets/NewChicken/Scripts/Fade/BreedBoard.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BreedBoard : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverUIObject())
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                if (hit.collider == this.gameObject.GetComponent<Collider2D>())
+                {
+                    GameObject.Find("EventSystem").GetComponent<Board>().Go_Breed();
+                }
+            }
+        }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
+}
